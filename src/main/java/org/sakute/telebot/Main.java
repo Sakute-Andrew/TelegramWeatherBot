16a092af4aa2f860690351d04b6467247b3f75e0
sltan <andriy.erdek1012@gmail.com>
2023-05-17 14:52:55 +0300

    1.1.1.1

--- src/main/java/org/sakute/telebot/Main.java
+++ src/main/java/org/sakute/telebot/Main.java
@@ -1,19 +1,30 @@
 package org.sakute.telebot;
 
-import org.sakute.telebot.persistance.dao.BlackListedUsersDao;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 import org.sakute.telebot.persistance.dao.UsersDao;
-import org.sakute.telebot.persistance.dao.impl.BlackListedUsersDaoImpl;
 import org.sakute.telebot.persistance.dao.impl.UsersDaoImpl;
 
-public class Main {
+public class Main extends Application {
 
   public static void main(String[] args) {
       UsersDao dao = new UsersDaoImpl();
       System.out.println(dao.getAllUsers());
-
-
-
-
+      launch();
 
   }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Scene scene = new Scene(new StackPane(l), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+
+    }
 }
